c1f48133ca7c3e3e3131007b435cd9884f09cccd
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2020-12-13 22:42:07 -0500

    Add files via upload

---
+++ src/test/java/com/mahen/springboot/angular/angular/AngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahen.springboot.angular.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
